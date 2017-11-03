3ed9edeb5a2c08fb4d9d7def14dab84e8eabc7d1
CallmePapa <1062927638@qq.com>
2017-11-03 18:11:26 +0800

    magifiyingGlass

--- image-magiifyingGlass/img-magifyingGlass.js
+++ image-magiifyingGlass/img-magifyingGlass.js
@@ -2,32 +2,28 @@
  * Created by weiqiujuan on 17-11-3.
  */
 $(document).ready(function () {
-    let vh = $(window).height;
-    let vw = $(window).width;
-
-    let imgH = $(".wrap img").height();
-    let imgW = $(".wrap img").width();
-
-    let beginX = vw * 2 / 10;
-    let endX = beginX + imgW;
-    let beginY = (vh - imgH) / 2;
-    let endY = beginY + imgH;
-    $(".wrap").css("margin-top", beginY + "px");
-
+    let vh = $(window).height()
+    vw = $(window).width()
+    let imgh = $(".wrap img").height()
+    let imgw = $(".wrap img").width()
+    let beginX = vw * 2 / 10
+    let endX = beginX + imgw
+    let beginY = (vh - imgh) / 2
+    let endY = beginY + imgh
+    $(".wrap").css("margin-top", beginY + "px")
     //鼠标经过
     document.addEventListener("mousemove", loupe, false);
     //触屏模式触发
     document.addEventListener("touchmove", loupe, false);
     document.addEventListener("touchstart", loupe, false);
-    document.addEventListener("touochend", function () {
+    document.addEventListener("touchend", function (e) {
         $(".loupe").css("visibility", "hidden");
     }, false);
-
     function loupe(e) {
-        let x, y;
-        if (e.type !== "mousemove") {
-            x = e.touches[0].pageX;
-            y = e.touches[0].pageY;
+        let x, y
+        if (e.type != "mousemove") {
+            x = e.touches[0].pageX
+            y = e.touches[0].pageY
         }
         //如果支持触摸事件，则屏蔽鼠标经过事件，避免影响touchstart的效果
         else if ("ontouchend" in document) {
@@ -35,19 +31,18 @@ $(document).ready(function () {
         }
         //如果不支持触摸事件，则让鼠标经过事件正常触发
         else {
-            x = e.clientX;
-            y = e.clientY;
+            x = e.clientX
+            y = e.clientY
         }
+        //判断鼠标或触摸点在图片区域，是则显示放大镜div层
         if (x > beginX && x < endX && y > beginY && y < endY) {
-            let mx = 100 - (x - beginX) / imgW * 1920//1920为原图宽度
-            let my = 100 - (y - beginY) / imgH * 1200//1200为原图高度
-
-            let bg = "url(http://wx.karlew.com/loupe/img/pic.jpg) " + mx + "px" + my + "px no-repeat #fff";
-            $(".loupe").css("left", x - 103 + "px").css("top", y - 103 + "px").css("background", bg);
-            $(".loupe").css("visibility", "visible");
+            let mx = 100 - (x - beginX) / imgw * 1920 //1920为原图片宽度
+            let my = 100 - (y - beginY) / imgh * 1200 //1200为原图片高度
+            let bg = "url(http://wx.karlew.com/loupe/img/pic.jpg) " + mx + "px " + my + "px no-repeat #fff"
+            $(".loupe").css("left", x - 103 + "px").css("top", y - 103 + "px").css('background', bg)
+            $(".loupe").css("visibility", "visible")
         } else {
-            $(".loupe").css("visibility", "hidden");
+            $(".loupe").css("visibility", "hidden")
         }
     }
-
 });
